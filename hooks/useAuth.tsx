34daaf7fb6e6889ddb2fbf84e3d1f697c6652569
myck-prog <myckland.learnprog@gmail.com>
2022-11-16 00:04:19 -0800

    authentication login page

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -10,12 +10,114 @@ import { useRouter } from "next/router";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { auth } from "../firebase";
 
-function useAuth() {
-  const [user, setUser] = useState();
-  return <div>useAuth</div>;
+interface IAuth {
+    user: User | null
+    signUp: (email: string, password: string) => Promise<void>
+    signIn: (email: string, password: string) => Promise<void>
+    logout: () => Promise<void>
+    error: string | null
+    loading: boolean
+  }
+
+const AuthContext = createContext<IAuth>({
+    user: null,
+    signUp: async () => {},
+    signIn: async () => {},
+    logout: async () => {},
+    error: null,
+    loading: false,
+  })
+
+interface AuthProviderProps{
+    children: React.ReactNode
 }
 
-export default useAuth;
+export const AuthProvider = ({children}: AuthProviderProps) => {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState(null)
+  const [initialLoading, setInitialLoading]=useState(true)
+  const router = useRouter();
+
+
+  useEffect(
+    () =>
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // Logged in...
+          setUser(user)
+          setLoading(false)
+        } else {
+          // Not logged in...
+          setUser(null)
+          setLoading(true)
+          router.push('/login')
+        }
+
+        setInitialLoading(false)
+      }),
+    [auth]
+  )
+
+  const signUp = async (email: string, password: string) => {
+    setLoading(true);
+
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        router.push("/");
+        setLoading(false);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  const signIn = async (email: string, password: string) => {
+    setLoading(true);
+
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        router.push("/");
+        setLoading(false);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  const logout = async () => {
+    setLoading(true);
+
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => alert(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  const memoedValue = useMemo(()=>({
+    user, signUp, signIn, loading, logout,error, 
+  }), [user, loading, ])
+
+
+  return (
+  <AuthContext.Provider  value = {memoedValue}>
+    {!initialLoading && children}
+  </AuthContext.Provider>
+  )
+  
+}
+export default function useAuth() {
+    return useContext(AuthContext)
+  }
+
 
 // This is a custom hook, usually return the state.
 // This will wrap the authentication state to the whole application
+// useMemo is a hook similar to useEffect, only recompute the memonized value when deps changed
+// Type of children in the Typescript is React.ReactNode StackOverflow
+// Type missing, in AuthContext.Provider, we need to provide value (better performance if use memoed Value)
+// useMemo, using the memoed Value, only recompute if either user or loading changed 
+// useEffect used to make the page persist if you log-on 
+// Creating a custom hook on AuthContext.Provider
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import useAuth from "../hooks/useAuth";
 
 interface Inputs {
   email: string;
@@ -11,19 +12,21 @@ interface Inputs {
 
 function Login() {
   const [login, setLogin] = useState(false);
+  const {signIn,signUp} = useAuth()
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = async({email,password}) => {
-    if(login){
-      // await SignIn(email, password)
-    } else{
-      // await signUp(email, password)
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (login) {
+       await signIn(email, password)
+    } else {
+      await signUp(email, password)
     }
-  }
+  };
 
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
@@ -53,22 +56,45 @@ function Login() {
         <div className="space-y-4">
           <label className="inline-block w-full ">
             {/* From the interface */}
-            <input type="email" placeholder="Email" className="input"
-            {...register('email',{required: true})} />
-             {errors.email && (<p className="p-1 text-[13px] font-light text-orange-500">Please enter a valid email</p>)}
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Please enter a valid email
+              </p>
+            )}
           </label>
           <label className="inline-block w-full ">
-            <input type="password" placeholder="Password" className="input" 
-            {...register('password', {required: true})}/>
-            {errors.password && (<p className="p-1 text-[13px] font-light text-orange-500">Your password must contain between 4 and 60 characters</p>)}
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Your password must contain between 4 and 60 characters
+              </p>
+            )}
           </label>
         </div>
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold" onClick={()=> setLogin(true)}>
+        <button
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}
+        >
           Sign In
         </button>
         <div className="text-[gray]">
           New to Netflix?{" "}
-          <button type="submit" className="text-white hover:underline"onClick={()=> setLogin(false)}>
+          <button
+            type="submit"
+            className="text-white hover:underline"
+            onClick={() => setLogin(false)}
+          >
             Sign up Now
           </button>
         </div>
@@ -82,4 +108,3 @@ export default Login;
 // Adding asynch function when user log into acc onSubmit
 // SubmitHandler destructuring data to email and password {}
 // Custom hook using signIn and signUp
-
