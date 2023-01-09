d735cac75664268d92139e4bc8adcb04f1b3453e
myck-prog <myckland.learnprog@gmail.com>
2023-01-08 16:43:01 -0800

    Modal complete

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,10 @@
-import { PlusIcon, ThumbUpIcon, VolumeOffIcon, VolumeUpIcon, XIcon } from "@heroicons/react/solid";
+import {
+  PlusIcon,
+  ThumbUpIcon,
+  VolumeOffIcon,
+  VolumeUpIcon,
+  XIcon,
+} from "@heroicons/react/solid";
 import MuiModal from "@mui/material/Modal";
 import { useEffect, useState } from "react";
 import { FaPlay } from "react-icons/fa";
@@ -77,9 +83,48 @@ function Modal() {
                 <ThumbUpIcon className="h-7 w-7" />
               </button>
             </div>
-            <button className="modalButton" onClick={()=>setMuted(!muted)}>
-              {muted ? <VolumeOffIcon className="h-6 w-6" /> :<VolumeUpIcon className="h-6 w-6"/>}
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <VolumeOffIcon className="h-6 w-6" />
+              ) : (
+                <VolumeUpIcon className="h-6 w-6" />
+              )}
             </button>
+          </div>
+        </div>
+
+        <div className="flex spacex-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 test-xs">
+                HD
+              </div>
+            </div>
+
+            {/* set medium breakpoint as side by side */}
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres?.map((genre) => genre.name).join(",")}
+                </div>
+                <div className="">
+                  <span className="text-[gray]">Original language: </span>
+                  {movie?.original_language}
+                </div>
+                <div className="">
+                  <span className="text-[gray]">Total Vote: </span>
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </>
