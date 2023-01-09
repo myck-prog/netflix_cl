6aa5463f5ef57914fec89ac7798dd590568774e8
myck-prog <myckland.learnprog@gmail.com>
2023-01-08 16:49:52 -0800

    Adding Fallback Videos

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -63,7 +63,7 @@ function Modal() {
         </button>
         <div className="relative pt-[56.25%]">
           <ReactPlayer
-            url={`https://www.youtube.com/watch?v=${trailer}`}
+            url={`https://www.youtube.com/watch?v=${trailer}`||`https://www.youtube.com/watch?v=WEhIRofi1z0&ab_channel=InsertBruh`}
             width="100%"
             height="100%"
             style={{ position: "absolute", top: "0", left: "0" }}
